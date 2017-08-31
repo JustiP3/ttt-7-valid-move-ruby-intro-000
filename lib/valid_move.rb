7992179f8b91ef2708b6deb76b01ba13bb1660ce
Justin Phillips <justinphil02@yahoo.com>
2017-08-31 00:33:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,14 +2,13 @@
 def valid_move?(array, index)
 
 x = position_taken?(array, index)
-if (index >= 0 && index < 9 && x == false)
+if (index > -1 && index < 9 && x == false)
   print "valid move"
   y = false
 end
 
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, index)
   if (array[index] == " " || array[index] == "" || array[index] == nil)
     x = false
